3541e0093c149f1294562cdd352ac64510cfa161
toolmind-deploys <toolminddeploys@gmail.com>
2025-06-07 15:20:23 -0700

    chore: add app/fuckyou/page.tsx

---
+++ app/fuckyou/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function FuckYouPage() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-4xl">FUCK you</div>
+    </main>
+  );
+}
