c3805df47ddb72e5cab643fa326d9bd745240dce
toolmind-deploys <toolminddeploys@gmail.com>
2025-06-07 15:20:25 -0700

    chore: add app/jason/page.tsx

---
+++ app/jason/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function JasonPage() {
+  return (
+    <main className="flex items-center justify-center h-screen">
+      <div className="text-4xl">1</div>
+    </main>
+  );
+}
